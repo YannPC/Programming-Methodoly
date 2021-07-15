67bb15190ea4fefe1306f892b166cb11f51d3bc3
YannPC <61549251+YannPC@users.noreply.github.com>
2021-07-15 23:50:11 +0200

    Add files via upload

---
+++ Doors.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.Door = void 0;
+var Door = /** @class */ (function () {
+    function Door(doorId) {
+        this.doorId = doorId;
+    }
+    return Door;
+}());
+exports.Door = Door;
