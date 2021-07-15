c571570d6c75177abb26fb328e2d39e0a0660f08
YannPC <61549251+YannPC@users.noreply.github.com>
2021-07-15 23:51:38 +0200

    Add files via upload

---
+++ MemberCard.ts
@@ -0,0 +1,82 @@
+import { Card } from "./Card";
+import { Door } from "./Doors";
+import { DoorNorth } from "./DoorNorth";
+import { DoorSouth } from "./DoorSouth";
+import { Zone } from "./Zone";
+
+export class MemberCard extends Card{
+  setZone(zone0: Zone) {
+      throw new Error("Method not implemented.");
+  }
+  private static currentId = 2000;
+ // private static employeeNumber:number;
+
+
+  /**
+   * Constructor for a Member's card
+   * @param {string} memberName - member's name
+   * @param {number} credits - inital number of credits
+   * @param {string} City - which City member registed
+   * @param {number} employeeNumber
+   * @param {string} type - type of the card (Staff ,Member)
+   */
+  constructor(
+    memberName: string,
+    credits: number,
+    employeeNumber:number,
+    City:string,
+    type: string,
+  ){
+    super(credits,City,memberName, type="Member")
+    this.cardId = MemberCard.currentId++;
+  }
+
+  /**
+   * @return {string} member's name
+   */
+  public getName = (): string => this.memberName;
+
+  /**
+   * @return {number} member's id
+   */
+  public getId = (): number => this.cardId;
+
+  /**
+   
+   * adds credits to the member's card
+   * @param {number} credits number of credits to be added
+   */
+  public addCredits = (credits: number): void => {
+    this.credits += credits;
+  };
+
+  /**
+   * @return {number} number of credits on the card
+   */
+
+  public getCredits = (): number => this.credits;
+  /**
+   * @return {number} number of credits on the card
+   */
+
+  /**
+   * @return {boolean} true if a card has enough credits to pass throught the Door,
+   * else false
+   */
+  public hasEnoughCredits = (): boolean => true;
+
+  /**
+   * @return {string} a String representation of the member card details
+   */
+  public toString = (): string =>
+    "***Member Card***" +
+    "\nCard No: " +
+    this.cardId +
+    "\nName: " +
+    this.memberName +
+    "\nCredits: " +
+    this.credits +
+    "\nCity : " +
+    this.City +
+    "\n";
+}
